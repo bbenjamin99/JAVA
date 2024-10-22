946b68f5d0e2aa3d5761bc1c008417c758ebe2e2
bbenjamin99 <bbenjamin4253@gmail.com>
2024-10-22 20:37:31 -0300

    finalizando

--- poo/src/EjemploAutomovilRelacionesObjetos.java
+++ poo/src/EjemploAutomovilRelacionesObjetos.java
@@ -2,14 +2,20 @@ public class EjemploAutomovilRelacionesObjetos {
     public static void main(String[] args) {
 
         Persona conductorAuto1 = new Persona("Benjamin", "Benitez");
+        Rueda[] ruedasNissan = new Rueda[5];
+        ruedasNissan[0] = new Rueda("Michelin", 26, 16.6);
+        ruedasNissan[1] = new Rueda("Michelin", 26, 16.6);
+        ruedasNissan[2] = new Rueda("Michelin", 26, 16.6);
+        ruedasNissan[3] = new Rueda("Michelin", 26, 16.6);
+        ruedasNissan[4] = new Rueda("Michelin", 26, 16.6);
 
-        Automovil auto1 = new Automovil();
-        auto1.setFabricante("Volkswagen");
-        auto1.setModelo("Gol Trend");
-        auto1.setMotor(new Motor(2.0d, TipoMotor.DIESEL));  
-        auto1.setEstanque(new Estanque(50));
+        Automovil nissan = new Automovil();
+        nissan.setFabricante("Nissan");
+        nissan.setModelo("Navara");
+        nissan.setMotor(new Motor(2.0d, TipoMotor.DIESEL));  
+        nissan.setEstanque(new Estanque(50));
 
-
+        Persona micaela = new Persona("Micaela", "De Luca");
         Automovil mazda = new Automovil();
         mazda.setFabricante("Honda");
         mazda.setModelo("Mazda");
@@ -17,6 +23,7 @@ public class EjemploAutomovilRelacionesObjetos {
         Motor motorMazda = new Motor();
         motorMazda.setTipo(TipoMotor.DIESEL);
         Estanque estanqueMazda = new Estanque(80); // Por defecto el estanque tiene 40.
-
+        
+        Automovil nissan2 = new Automovil("Nissan", "Navara2", "Blanco",new Motor(2.0,TipoMotor.DIESEL), new Estanque(), micaela, new Rueda[4]);
     }
 }
