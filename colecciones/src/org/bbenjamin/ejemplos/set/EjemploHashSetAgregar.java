6f87cb6a4e2b1d635ef0f5e0835226829b1f9503
bbenjamin99 <bbenjamin4253@gmail.com>
2024-11-12 20:37:01 -0300

    Ejemplo Hashset detectando duplicados

--- colecciones/src/org/bbenjamin/ejemplos/set/EjemploHashSetAgregar.java
+++ colecciones/src/org/bbenjamin/ejemplos/set/EjemploHashSetAgregar.java
@@ -6,7 +6,19 @@ public class EjemploHashSetAgregar {
     public static void main(String[] args) {
         
         HashSet<String> hs = new HashSet<>();
+        hs.add("Uno");
+        hs.add("Dos");
+        hs.add("Tres");
+        hs.add("Cuatro");
+        hs.add("Cinco");
+        
+        System.out.println(hs);
 
+        boolean b = hs.add("Tres");
 
+        System.out.println(hs);
+        System.out.print("Permite elementos duplicados? :\t");
+        System.out.println(b);
     }
 }
+ 
