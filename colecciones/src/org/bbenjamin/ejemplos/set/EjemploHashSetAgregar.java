977f0bf9766ee09110af7607efee2aeb873823d6
bbenjamin99 <bbenjamin4253@gmail.com>
2024-11-12 13:54:04 -0300

    Comenzando con lists

---
+++ colecciones/src/org/bbenjamin/ejemplos/set/EjemploHashSetAgregar.java
@@ -0,0 +1,12 @@
+package org.bbenjamin.ejemplos.set;
+
+import java.util.HashSet;
+ 
+public class EjemploHashSetAgregar {
+    public static void main(String[] args) {
+        
+        HashSet<String> hs = new HashSet<>();
+
+
+    }
+}
