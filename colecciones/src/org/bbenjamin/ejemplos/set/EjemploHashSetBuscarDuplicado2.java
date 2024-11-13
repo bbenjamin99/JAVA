1684360b73bfc8eaf3e3a8ec3f3389d5f5a9455a
bbenjamin99 <bbenjamin4253@gmail.com>
2024-11-12 21:33:36 -0300

    Entrando a los TreeSet

---
+++ colecciones/src/org/bbenjamin/ejemplos/set/EjemploHashSetBuscarDuplicado2.java
@@ -0,0 +1,24 @@
+package org.bbenjamin.ejemplos.set;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class EjemploHashSetBuscarDuplicado {
+    public static void main(String[] args) {
+        
+        String[] peces = {"Corvina", "Lenguado", "Pejerrey", "Robalo", "Atun", "Lenguado"};
+
+        Set<String> unicos = new HashSet<>();
+        Set<String> duplicados = new HashSet<>();
+        for(String pez : peces) {
+
+            if(!unicos.add(pez)){
+                duplicados.add(pez);
+                System.out.println("Elementos duplicados : " + pez);
+            }
+        }
+        unicos.removeAll(duplicados);
+        System.out.println("Elementos unicos: " + unicos);
+        System.out.println("Elementos duplicados: " + duplicados);
+    }
+}
