f290c743fedb7163868112429a07a4435a203dfd
bbenjamin99 <bbenjamin4253@gmail.com>
2024-11-29 15:11:34 -0300

    finalizando expresiones lambda

--- java8_lambda/src/org/bbenjamin/java8/lambda/EjemploConsumer.java
+++ java8_lambda/src/org/bbenjamin/java8/lambda/EjemploConsumer.java
@@ -1,10 +1,15 @@
 package org.bbenjamin.java8.lambda;
 
+import org.bbenjamin.java8.lambda.models.Usuario;
+
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 public class EjemploConsumer {
     public static void main(String[] args) {
@@ -18,15 +23,30 @@ public class EjemploConsumer {
         };
         consumidor1.accept(new Date());
 
-        BiConsumer<String, Integer> consumidorBI = (String nombre, Integer edad) -> {
-            System.out.println(nombre + " Tiene " + edad + " años");
-        };
+        BiConsumer<String, Integer> consumidorBI = (String nombre, Integer edad) -> System.out.println(nombre + " Tiene " + edad + " años");
+
         consumidorBI.accept("Benjamin", 25 );
 
+            Consumer<String> consumidor2 =  System.out::println;
+        consumidor2.accept("Hello World!");
+
+        List<String> nombres = Arrays.asList("Benjamin", "Benitez", "Rogelio","Pepe", "Paco");
+
+        nombres.forEach(consumidor2);
+
+        Supplier<Usuario> crearUsuario = Usuario::new;
+            Usuario usuario = crearUsuario.get();
+
+        BiConsumer<Usuario, String> asignarNombre = Usuario::setNombre;
+
+            asignarNombre.accept(usuario, "Ricardito");
+
+        System.out.println("Nombre usuario: " + usuario.getNombre());
+
+        Supplier<String> proveedor = () ->  "Hello world";
 
 
-
-
+        System.out.println( proveedor.get() );
 
 
 
---
+++ java8_lambda/src/org/bbenjamin/java8/lambda/EjemploFunction.java
@@ -0,0 +1,26 @@
+package org.bbenjamin.java8.lambda;
+
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+public class EjemploFunction {
+    public static void main(String[] args) {
+
+        Function<String, String> f1 = param -> "Hola que tal! " + param;
+
+        String resultado  = f1.apply("Benjamin");
+        System.out.println(resultado);
+
+        Function<String, String> f2 =  String::toUpperCase;
+        System.out.println(f2.apply("Como va chavales!"));
+
+        BiFunction<String, String, String> f3 = (a,b) -> a.toUpperCase().concat(b.toUpperCase());
+
+        String resultado2 = f3.apply("hola buenos días", "chau buenas noches");
+        System.out.println(resultado2) ;
+
+        BiFunction<String, String, Integer> f4 = String::compareTo;
+        System.out.println(f4.apply("Benjamin", "Benjamin")) ;
+
+    }
+}
